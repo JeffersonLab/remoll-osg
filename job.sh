424797e650246904362825aae469ceff89dd9cb8
Yug34 <vertilax1337@gmail.com>
2022-01-19 22:32:37 +0530

    Added owner

--- job.sh
+++ job.sh
@@ -3,11 +3,11 @@
 source /etc/profile
 source /jlab/remoll/bin/remoll.sh
 
-#curl https://github.com/JeffersonLab/remoll/blob/$3/geometry/$4 -o $4
+#curl https://github.com/$3/remoll/blob/$4/geometry/$5 -o $5
 
 remoll /srv/runexample.mac
 mv /srv/remollout.root /srv/remollout.$1.$2.root
 
-#rm $4
+#rm $5
 
 stashcp /srv/remollout.$1.$2.root stash:///osgconnect/public/${osgName}/remollOutput/remollout.$1.$2.root
