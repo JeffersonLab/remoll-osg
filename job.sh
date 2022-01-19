8ca8ef0128acbcbeb30a682f83c89496fa28a5d2
Yug34 <vertilax1337@gmail.com>
2022-01-19 22:28:33 +0530

    Added ability to download geometry from github branches

--- job.sh
+++ job.sh
@@ -3,7 +3,11 @@
 source /etc/profile
 source /jlab/remoll/bin/remoll.sh
 
+#curl https://github.com/JeffersonLab/remoll/blob/$3/geometry/$4 -o $4
+
 remoll /srv/runexample.mac
 mv /srv/remollout.root /srv/remollout.$1.$2.root
+
+#rm $4
 
 stashcp /srv/remollout.$1.$2.root stash:///osgconnect/public/${osgName}/remollOutput/remollout.$1.$2.root
